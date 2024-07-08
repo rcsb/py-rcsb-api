bfbda0d5b045c3c5f7c2f47668bfe824eb08e5aa
habiba-m <habibahussamkamal@gmail.com>
2024-07-08 14:24:54 -0400

    fixed test query linting errors

--- tests/test_data_query.py
+++ tests/test_data_query.py
@@ -1,8 +1,8 @@
 ##
 # File:    testquery.py
-# Author:  
-# Date:    
-# Version: 
+# Author:
+# Date:
+# Version:
 #
 # Update:
 #
@@ -18,17 +18,17 @@ __email__ = ""
 __license__ = ""
 
 import logging
-import importlib
+# import importlib
 # import platform
 # import resource
 import time
 import unittest
 import requests
 
-from rcsbapi.data import query
+# from rcsbapi.data import query
 from rcsbapi.data.query import Query
-from rcsbapi.data.query import PDB_URL
-from rcsbapi.data.query import SCHEMA
+# from rcsbapi.data.query import PDB_URL
+# from rcsbapi.data.query import SCHEMA
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
 logger = logging.getLogger()
@@ -45,21 +45,20 @@ class QueryTests(unittest.TestCase):
         logger.info("Completed %s at %s (%.4f seconds)", self.id().split('.')[-1], time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
 
     def testGetEditorLink(self):
-        query_str = '{ entries(entry_ids: ["4HHB", "1IYE"]) {\n  exptl {\n     method_details\n     method\n     details\n     crystals_number\n  }\n}}'
+        # query_str = '{ entries(entry_ids: ["4HHB", "1IYE"]) {\n  exptl {\n     method_details\n     method\n     details\n     crystals_number\n  }\n}}'
         query_obj = Query({"entry_ids": ["4HHB","1IYE"]}, "entries",["exptl"])
         url = query_obj.get_editor_link()
         response_json = requests.GET(url)
         self.assertEqual(response_json.status_code, 200)
-
 
     def testPostQuery(self):
         with self.subTest("1. Batching into requests with fewer Ids"):
             input_ids = []
             for i in range(165):
                 input_ids.append("4HHB")
-            query_obj = Query({"entry_ids": input_ids}, "entries",["exptl"])
+            query_obj = Query({"entry_ids": input_ids}, "entries", ["exptl"])
             query_obj.post_query()
-            #assert that the batch and merge functions are called
+            # assert that the batch and merge functions are called
 
     def testParseGQLError(self):
         pass
@@ -68,7 +67,7 @@ class QueryTests(unittest.TestCase):
         input_ids = []
         for i in range(165):
             input_ids.append("4HHB")
-        query_obj = Query({"entry_ids": input_ids}, "entries",["exptl"])
+        query_obj = Query({"entry_ids": input_ids}, "entries", ["exptl"])
         batch_size = 50
         batched_ids = query_obj.batch_ids(batch_size)
         total_ids = 0
@@ -79,7 +78,7 @@ class QueryTests(unittest.TestCase):
         self.assertEqual(len(query_obj.input_ids_list), total_ids)
 
     def testMergeResponse(self):
-        #assert that the lengths are combined and all ids are present?
+        # assert that the lengths are combined and all ids are present?
         pass
 
     def testEditortoQuery(self):
