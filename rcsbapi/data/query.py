de77681adb00dde754fa9c96e37c29d7ffc67677
habiba-m <habibahussamkamal@gmail.com>
2024-07-08 14:13:03 -0400

    fixed linting errors in query.py

--- rcsbapi/data/query.py
+++ rcsbapi/data/query.py
@@ -1,5 +1,5 @@
-from rcsbapi.data import schema
 import requests
+from rcsbapi.data import schema
 import logging
 import time
 import urllib.parse
@@ -11,16 +11,17 @@ SCHEMA = schema.Schema(PDB_URL)
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s]: %(message)s")
 
+
 class Query:
 
-    def __init__(self, input_ids: Union[List[str],Dict[Any, Any]], input_type: str, return_data_list: List[str]):  # TODO: will edit when dicts and strings for input are supported
+    def __init__(self, input_ids: Union[List[str], Dict[Any, Any]], input_type: str, return_data_list: List[str]):
         input_id_limit = 300
         if len(input_ids) > input_id_limit:
             raise ValueError(f"Too many input_ids. Reduce to less than {input_id_limit}.")
         self.input_ids = input_ids
         self.input_type = input_type
         self.return_data_list = return_data_list
-        self.query = SCHEMA.construct_query(input_ids, input_type, return_data_list)
+        self.query = SCHEMA.construct_query(input_type, return_data_list, input_ids)
         self.plural_input = False
         if SCHEMA.root_dict[self.input_type][0]["kind"] == "LIST":
             self.plural_input = True
@@ -28,7 +29,7 @@ class Query:
                 self.input_ids_list: List[str] = input_ids[SCHEMA.root_dict[self.input_type][0]["name"]]
             if isinstance(input_ids, list):
                 self.input_ids_list = input_ids
-        self.response = self.post_query()  # TODO: should this be upon initialization or must be called explicitly?
+        self.response = self.post_query()
 
     def get_input_ids(self):
         return self.input_ids
@@ -54,10 +55,10 @@ class Query:
         if (self.plural_input is True) and (len(self.input_ids_list) > batch_size):
             batched_ids = self.batch_ids(batch_size)
             response_json = {}
-            count = 0
+            # count = 0
             for id_batch in batched_ids:
-                query = re.sub(r"\[([^]]+)\]", f"{id_batch}".replace("\'","\""), self.query)
-                part_response =  requests.post(headers={"Content-Type": "application/graphql"}, data=query, url=PDB_URL).json()
+                query = re.sub(r"\[([^]]+)\]", f"{id_batch}".replace("\'", "\""), self.query)
+                part_response = requests.post(headers={"Content-Type": "application/graphql"}, data=query, url=PDB_URL, timeout=10).json()
                 self.parse_gql_error(part_response)
                 time.sleep(0.2)
                 if not response_json:
@@ -65,7 +66,7 @@ class Query:
                 else:
                     response_json = self.merge_response(response_json, part_response)
         else:
-            response_json = requests.post(headers={"Content-Type": "application/graphql"}, data=self.query, url=PDB_URL).json()
+            response_json = requests.post(headers={"Content-Type": "application/graphql"}, data=self.query, url=PDB_URL, timeout=10).json()
             self.parse_gql_error(response_json)
         if "data" in response_json.keys():
             query_response = response_json["data"][self.input_type]
@@ -73,10 +74,10 @@ class Query:
                 logging.warning("Input produced no results. Check that input ids are valid")
             if isinstance(query_response, list):
                 if len(query_response) == 0:
-                   logging.warning("Input produced no results. Check that input ids are valid")        
+                    logging.warning("Input produced no results. Check that input ids are valid")        
         return response_json
-            # parse_response(response_json)
-            # fields_list = 
+        # parse_response(response_json)
+        # fields_list=
 
     def parse_gql_error(self, response_json):
         if "errors" in response_json.keys():
@@ -88,8 +89,7 @@ class Query:
                     combined_error_msg += f"{i+1}. {error_msg_list[i]}"
                     raise ValueError(f"{combined_error_msg}.\n Run <query object name>.get_editor_link() to get a link to GraphiQL editor with query")
 
-    #TODO: change to use list of input ids?
-    def batch_ids(self, batch_size) -> List[Dict[str, List[str]]]: # assumes that plural types have only one arg, which is true right now
+    def batch_ids(self, batch_size) -> List[Dict[str, List[str]]]:  # assumes that plural types have only one arg, which is true right now
         id_name = SCHEMA.root_dict[self.input_type][0]["name"]
         batched_ids: List[Dict[str, List[str]]] = []
         i = 0
@@ -107,4 +107,4 @@ class Query:
     def merge_response(self, merge_into_response, to_merge_response):
         combined_response = merge_into_response
         combined_response["data"][self.input_type] += to_merge_response["data"][self.input_type]
-        return combined_response  
+        return combined_response
