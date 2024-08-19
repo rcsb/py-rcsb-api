3022f6d9db262ac9fdb840559c023418e326a577
Ivana Truong <ivanatruo@gmail.com>
2024-08-19 13:11:31 -0500

    add to requirements

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("../rcsbapi"))
-sys.path.insert(0, os.path.abspath("../rcsbapi/docs"))
 import rcsbapi  # noqa: E402
 
 # -- Project information -----------------------------------------------------
