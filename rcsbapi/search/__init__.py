dc3b4b47f649db6a7e393e19cb2f9f4460a2bf76
Ivana Truong <ivanatruo@gmail.com>
2024-10-07 13:58:50 -0700

    copying over rcsbsearchapi files

---
+++ rcsbapi/search/__init__.py
@@ -0,0 +1,23 @@
+"""RCSB PDB Search API"""
+
+from typing import List
+from .search import Terminal, SCHEMA  # noqa: F401
+from .search import Attr, AttributeQuery, Group, Query, TextQuery
+
+__version__ = "2.0.0"
+
+rcsb_attributes = SCHEMA.rcsb_attributes
+
+
+def __dir__() -> List[str]:
+    return sorted(__all__)
+
+
+__all__ = [
+    "Group",
+    "Attr",
+    "Terminal",
+    "TextQuery",
+    "AttributeQuery",
+    "rcsb_attributes"
+]
