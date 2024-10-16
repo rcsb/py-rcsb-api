bcccfab31333828f09b7d87d4f07825275e2e875
Dennis Piehl <piehld@gmail.com>
2024-10-16 17:50:47 -0400

    update config.py

--- rcsbapi/config.py
+++ rcsbapi/config.py
@@ -23,9 +23,11 @@ class Config:
     SUPPRESS_AUTOCOMPLETE_WARNING: bool = False
 
     def __setattr__(self, name, value):
+        """Override `__setattr__` to verify attribute exists when a user tries to set a configuration parameter.
+        Raises an error if users accidentally try to create a new, unused attribute (e.g., due to a typo or misspelling).
+        """
         if not hasattr(self, name):
-            logger.error("Attribute name '%s' not a member of Config class", name)
-            # raise AttributeError(f"Attribute name '{name}' not a member of Config class")
+            raise AttributeError(f"'{name}' is not a valid attribute of Config class")
         super().__setattr__(name, value)
 
 
