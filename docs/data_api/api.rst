7162d18a95fb7f5c15662e20e215f044d27fc68a
Ivana Truong <ivanatruo@gmail.com>
2025-03-13 10:24:29 -0500

    change api.rst

--- docs/data_api/api.rst
+++ docs/data_api/api.rst
@@ -4,3 +4,9 @@ API Documentation
 .. automodule:: rcsbapi.data
     :members:
     :special-members: __init__
+
+.. automodule:: rcsbapi.data.data_query
+    :members:
+
+.. automodule:: rcsbapi.data.data_schema
+    :members:
--- rcsbapi/data/__init__.py
+++ rcsbapi/data/__init__.py
@@ -5,26 +5,26 @@ DATA_SCHEMA = DataSchema()
 
 # This is needed because __getattr__ will be called twice on import,
 # so ALL_STRUCTURES should be cached to avoid initializing twice
-# _import_cache: dict = {}
+_import_cache: dict = {}
 
 
-# def __getattr__(name: str):
-#     """Overloading __getattr__ so that when ALL_STRUCTURES is accessed for the first time,
-#     ALL_STRUCTURES object will be built.
+def __getattr__(name: str):
+    """Overloading __getattr__ so that when ALL_STRUCTURES is accessed for the first time,
+    ALL_STRUCTURES object will be built.
 
-#     Args:
-#         name (str): attribute name
-#     """
-#     if name == "ALL_STRUCTURES":
-#         if name not in _import_cache:
-#             from .data_query import AllStructures
-#             ALL_STRUCTURES = AllStructures()
-#             _import_cache[name] = ALL_STRUCTURES
+    Args:
+        name (str): attribute name
+    """
+    if name == "ALL_STRUCTURES":
+        if name not in _import_cache:
+            from .data_query import AllStructures
+            ALL_STRUCTURES = AllStructures()
+            _import_cache[name] = ALL_STRUCTURES
 
-#         return _import_cache[name]  # Return cached instance
+        return _import_cache[name]  # Return cached instance
 
-#     # keep functionality of original __getattr__
-#     raise AttributeError(f"Module {repr(__name__)} has no attribute {repr(name)}")
+    # keep functionality of original __getattr__
+    raise AttributeError(f"Module {repr(__name__)} has no attribute {repr(name)}")
 
 
 from .data_query import DataQuery  # noqa:E402
