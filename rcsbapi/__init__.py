8332d8b88d162670f393b10bed25369fbbcbdb46
Ivana Truong <ivanatruo@gmail.com>
2025-03-20 14:49:41 -0500

    update CHANGELOG and init

--- rcsbapi/__init__.py
+++ rcsbapi/__init__.py
@@ -2,7 +2,7 @@ __docformat__ = "restructuredtext en"
 __author__ = "Dennis Piehl"
 __email__ = "dennis.piehl@rcsb.org"
 __license__ = "MIT"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
