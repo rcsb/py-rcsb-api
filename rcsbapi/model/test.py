e31bb4278e0f2fee3546c74fb41dfd9bf1f31be7
Krish <krishparmar69212@gmail.com>
2025-07-22 17:10:48 -0400

    File Deletion

--- rcsbapi/model/test.py
+++
@@ -1,4 +0,0 @@
-from rcsbapi.model import ModelQuery
-query = ModelQuery(compress_gzip=True, download=True, encoding='bcif', file_directory="model-output")  # setting 'bcif' up front, not propagated to result
-result = query.get_assembly(entry_id="13PK", name="3")
-print(result)
